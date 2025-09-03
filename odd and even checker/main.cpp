f9bf9c1ff10904557f186572ee898ae80d03bb4b
MatteoCode <165672723+CodeoMatteo@users.noreply.github.com>
2025-09-03 20:39:23 +0800

    Add files via upload

---
+++ odd and even checker/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << "This is a even number" << endl;
+    } else {
+        cout << "This is a odd number" << endl;
+    }
+
+    return 0;
+}
